ec65c197c86ab16119bec5f9c378d35351cfc6d0
linzars2006-oss <linzars2006@gmail.com>
2026-09-18 19:41:53 +0530

    Update MongoDB connection string to use Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/appointment-booking');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://linzars2006_db_user:SmartVisitor@2026#DB@cluster0.aejipkb.mongodb.net/?appName=Cluster0');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
